7ff52356d0f0951208acf49c786e815dab88de8e
baidu <baidu@baidudeMacBook-Pro.local>
2015-03-13 14:43:16 +0800

    fix a bug for heartbeat

--- cup/services/heartbeat.py
+++ cup/services/heartbeat.py
@@ -17,8 +17,11 @@
 """
 
 import time
+import pickle
+
 from cup import log
 from cup import net
+from cup.util import conf
 from cup.res import linux
 
 try:
@@ -40,6 +43,7 @@ class Device(object):
         """
         self._name = name
         self._last_healthy = time.time()
+        self._dict_info = None
 
     def get_last_healthy(self):
         """
@@ -54,9 +58,22 @@ class Device(object):
         log.debug('device %s set_last_healthy' % self._name)
         self._last_healthy = time.time()
 
+    def serilize(self):
+        """
+        serilize device info
+        """
+        return pickle.dumps(self._dict_info)
+
+    def get_name(self):
+        """get name"""
+        return self._name
+
 
 class LinuxHost(Device):
-    def __init__(self, name, init_this_host=False):
+    """
+    a linux host resource
+    """
+    def __init__(self, name, init_this_host=False, iface='eth0'):
         """
         :param name:
             name of the LinuxHost
@@ -67,27 +84,35 @@ class LinuxHost(Device):
         super(self.__class__, self).__init__(name)
         # -1 means initialized
         self._dict_info = {
+            'iface':   iface,
             'ipaddr': '0.0.0.0',
+            'hostname': net.get_local_hostname(),
             'cpu_idle': -1,
             'mem_inuse': -1,        # MB
             'mem_total': -1,
-            'net_in':    -1,        # kb
-            'net_out':   -1,      # kb
+            'net_in': -1,        # kb
+            'net_out': -1      # kb
         }
 
         if init_this_host:
             self._dict_info['ipaddr'] = net.get_hostip()
-            cupinfo = linux.get_cpu_usage(0.5)
+            cpuinfo = linux.get_cpu_usage(1)
             meminfo = linux.get_meminfo()
-            # TODO   which one is the default network interface
-            netinfo = linux.get_net_recv_speed(eth0)
+            self._dict_info['net_in'] = linux.get_net_recv_speed(
+                self._dict_info['iface'], 1
+            )
+            self._dict_info['net_out'] = linux.get_net_transmit_speed(
+                self._dict_info['iface'], 1
+            )
+            # pylint: disable=E1101
             self._dict_info['cpu_idle'] = cpuinfo.idle
+            # pylint: disable=E1101
             self._dict_info['mem_inuse'] = meminfo.total - meminfo.free
-
 
     def set_linux_res_bydict(self, info_dict):
         """
             {
+                'iface': 'eth0',
                 'ipaddr': '10.10.10.1',
                 'port':   8089,
                 'cpu_idle': 50,
@@ -168,9 +193,9 @@ class LinuxHost(Device):
         return (self._dict_info['net_in'], self._dict_info['net_out'])
 
 
-class Process(LinuxHost):
-    def __init__(self, process_name):
-        self.
+# class Process(LinuxHost):
+#     def __init__(self, procname, path):
+
 
 class HeartbeatService(object):
     """
@@ -229,11 +254,14 @@ class HeartbeatService(object):
         return True
 
     def get_lost(self):
+        """
+        get lost devices
+        """
         now = time.time()
         lost_devices = []
         for dkey in self._devices.keys():
-            device = self._devices['dkey']
-            if device.get_last_healthy() - now > self._judge_lost:
+            device = self._devices[dkey]
+            if now - device.get_last_healthy() > self._judge_lost:
                 if self._lost_devices is not None:
                     self._lost_devices[dkey] = device
                 del self._devices[dkey]
@@ -257,19 +285,35 @@ class HeartbeatService(object):
             self._lost_devices = {}
             log.info('end - does not have dump_file, return')
             return
-        try:
-            with open(dump_file, 'w+') as fhandle:
-                for dkey in self._lost_devices.keys():
-                    fhandle.write(
-                        'key:%s last_healthy:%d' % (
-                            dkey, self._devices[dkey].get_last_healthy()
-                        )
-                    )
-                    del self._lost_devices[dkey]
-            log.info('end - empty_lost devices')
-        except IOError as ioerror:
-            log.warn('failed to dump lost_file, error:%s' % str(ioerror))
+        info_dict = {}
+        info_dict['devices'] = {}
+        if len(self._lost_devices) != 0:
+            info_dict['devices']['lost'] = []
+            info_dict['devices']['lost_num'] = len(self._lost_devices)
+        else:
+            info_dict['devices']['lost_num'] = 0
+        for dkey in self._lost_devices.keys():
+            try:
+                tmp_dict = {}
+                tmp_dict['key'] = dkey
+                tmp_dict['last_healthy'] = self._devices[dkey].get_last_healthy()
+                del self._lost_devices[dkey]
+                log.info('end - empty_lost devices')
+                info_dict['devices']['lost'].append(tmp_dict)
+            except KeyError as error:
+                log.warn('failed to dump lost_file, error:%s' % str(error))
+        conf_writer = conf.Dict2Configure(info_dict)
+        conf_writer.write_conf(dump_file)
         return
 
 
+def _test():
+    localhost = LinuxHost(name='localhost', init_this_host=True)
+    binary = localhost.serilize()
+    print 'binary:%s' % binary
+    print pickle.loads(binary)
+
+
+if __name__ == '__main__':
+    _test()
 # vi:set tw=0 ts=4 sw=4 nowrap fdm=indent
