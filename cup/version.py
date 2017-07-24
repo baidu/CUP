ddeb1bba48e9a7e9a43143556ae5ec42154a0577
Guannan Ma <mythmgn@gmail.com>
2017-07-24 11:09:41 +0800

    Change lib version

--- cup/version.py
+++ cup/version.py
@@ -2,20 +2,16 @@
 # -*- coding: utf-8 -*
 # #############################################################################
 #
-#  Copyright (c) 2014 Baidu.com,  Inc. All Rights Reserved
+#  Copyright (c) Baidu.com,  Inc. All Rights Reserved
 #
 # #############################################################################
 
 """
 :author:
     Guannan Ma maguannan @mythmgn
-:create_date:
-    2014
-:modify_date:
-    2017.7.21
 """
 
 VERSION = '1.5.6'
 AUTHOR = 'nfs-qa'
 
-# vi:set tw=0 ts=4 sw=4 nowrap fdm=indent 
+# vi:set tw=0 ts=4 sw=4 nowrap fdm=indent
