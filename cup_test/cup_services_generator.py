5e4330cb2e4ccdc67ad94f0084e745eed6f96d6b
maguannan <mythmgn@gmail.com>
2018-09-30 21:32:23 +0800

    fix decorator docstring bug

---
+++ cup_test/cup_services_generator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+# Copyright: [CUP] - See LICENSE for details.
+# Authors: Guannan Ma (@mythmgn),
+"""
+:description:
+
+"""
+
+import os
+import sys
+import time
+
+from cup import cache
+from cup import unittest
+from cup.services import generator
+
+
+def test_generator():
+    a_gen = generator.CGeneratorMan()
+    b_gen = generator.CGeneratorMan()
+    print a_gen
+    print b_gen
+    assert a_gen == b_gen
+
+
+# vi:set tw=0 ts=4 sw=4 nowrap fdm=indent
