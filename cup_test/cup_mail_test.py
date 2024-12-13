937935f4603b2a37c6e35c4b440bdee5a57aebb6
guannanma <guannanma@didiglobal.com>
2024-12-14 00:00:05 +0800

    ## Version 3.2.34
    * cup.services.generator
      * [new] replace CGeneratorMan with new class Py3Generator
      * [compatibility] cup.servcies.generator, unicode<>str in py3/py2
      * [enhancement]
    * cup.timeplus
      * [new] add timestamp2datetime for converting timestamp to datetime object
    * cup.cache
      * [new] add mapupdate function for KVMemCache
      * [bug] fix bug for KVMemCache.expire
    * cup.mail
      * [enhancement] add Smtp with ssl support
    * cup.storage.obj s3 object apis
      * [new] Add ssl verify support with local public crt file
      * [enhancement] Change open mode from unspecified to binary mode. r -> rb; w+ -> wb

---
+++ cup_test/cup_mail_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+# Copyright: See LICENSE for details.
+# Authors: Guannan Ma (@mythmgn),
+"""
+:description:
+
+"""
+
+
+# vi:set tw=0 ts=4 sw=4 nowrap fdm=indent
